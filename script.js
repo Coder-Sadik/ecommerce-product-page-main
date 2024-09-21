f0702e53867dca2b463a74ecd9aa1af8a57b2be2
Sadik Rahman <srahman2702@gmail.com>
2024-09-21 20:26:17 +0600

    mobile design completed

--- script.js
+++ script.js
@@ -15,22 +15,3 @@ hamburger.addEventListener("click", toggleMenu);
 // Toggle menu on clicking the close icon or overlay
 closeIcon.addEventListener("click", toggleMenu);
 overlay.addEventListener("click", toggleMenu);
-
-// img slider
-// let currentIndex = 0;
-// const images = [
-// 	"image-product-1.jpg",
-// 	"image-product-2.jpg",
-// 	"image-product-3.jpg",
-// 	"image-product-4.jpg",
-// ];
-
-// const changeImage = (direction) => {
-// 	currentIndex += direction;
-// 	if (currentIndex < 0) {
-// 		currentIndex = images.length - 1;
-// 	} else if (currentIndex >= images.length) {
-// 		currentIndex = 0;
-// 	}
-// 	document.getElementById("slider-image").src = images[currentIndex];
-// };
