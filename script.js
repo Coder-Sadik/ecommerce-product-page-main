d22ee2113f64708ef481d1da612a42efe6fc6611
Sadik Rahman <srahman2702@gmail.com>
2024-09-23 18:06:43 +0600

    javascript funtionality added

--- script.js
+++ script.js
@@ -27,3 +27,33 @@ hamburger.addEventListener("click", toggleMenu);
 // Toggle menu on clicking the close icon or overlay
 closeIcon.addEventListener("click", toggleMenu);
 overlay.addEventListener("click", toggleMenu);
+
+//quantity control
+const setQuantity = document.getElementById("quantity");
+let quantity = 0;
+const quantityControl = (n) => {
+	quantity += n;
+	quantity >= 0 ? (setQuantity.innerText = quantity) : (quantity = 0);
+};
+
+//carousel
+let images = [
+	"image-product-1.jpg",
+	"image-product-2.jpg",
+	"image-product-3.jpg",
+	"image-product-4.jpg",
+];
+const maxLength = images.length;
+const sliderImage = document.getElementById("slider-image");
+
+//update image of carousel
+const updateImage = (n) => {
+	sliderImage.src = "images/" + images[n];
+};
+
+//button controlled carousel
+let currentIndex = 0;
+const updateIndex = (n) => {
+	currentIndex = (currentIndex + maxLength + n) % maxLength;
+	updateImage(currentIndex);
+};
